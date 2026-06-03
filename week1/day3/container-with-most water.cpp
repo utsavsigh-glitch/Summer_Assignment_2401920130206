6a045b5216e9c1008a91001286c0940e6035947f
Utsav Singh <utsavsigh@gmail.com>
2026-06-03 22:57:15 +0530

    Refactor maxArea function to use 'ans' variable

--- week1/day3/container-with-most water.cpp
+++ week1/day3/container-with-most water.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int maxWater = 0;
+        int ans = 0;
 
-        for (int i = 0; i < height.size(); i++) {
-            for (int j = i + 1; j < height.size(); j++) {
-                int width = j - i;
-                int h = min(height[i], height[j]);
-                int area = width * h;
-
-                maxWater = max(maxWater, area);
+        for(int i = 0; i < height.size(); i++) {
+            for(int j = i + 1; j < height.size(); j++) {
+                int water = min(height[i], height[j]) * (j - i);
+                ans = max(ans, water);
             }
         }
 
-        return maxWater;
+        return ans;
     }
 };

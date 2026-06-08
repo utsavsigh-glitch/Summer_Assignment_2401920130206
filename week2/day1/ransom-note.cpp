919941e93b8d581c8333f0826db03553748360a3
Utsav Singh <utsavsigh@gmail.com>
2026-06-08 23:44:45 +0530

    Add ransom-note solution class

---
+++ week2/day1/ransom-note.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        for(int i=0;i<ransomNote.size();i++) {
+            int pos= magazine.find(ransomNote[i]);
+            if(pos ==string::npos){
+                return false;
+            }
+            magazine.erase(pos,1);
+        }
+        return true;
+    }
+};
